9bef4aa85e14d83c8bde0704901e6ff569bf71d2
Lorena Oliveira <51932176+lorena-767@users.noreply.github.com>
2021-12-12 19:02:36 -0300

    Atividade encontro remoto 2
    Ajustes no código

--- sistemaw.js
+++ sistemaw.js
@@ -1,8 +1,13 @@
+
+//4º exemplo
 let nomeDeAluno = ["MARCEL", "LORENA", "LUANA"]
 
 for (let nome of nomeDeAluno){
     console.log(`essa pessoa se chama ${nome}`)
 }
+-----------------------------------------------------------------------------
+
+//3º exemplo
 
 //let numeroDeAlunos = 10;
 
@@ -21,7 +26,30 @@ for (let nome of nomeDeAluno){
       //  console.log(`O número ${contador}é par`)
     //}
        //contador++; 
+   //}
+------------------------------------------------------------------------------------------------
 
+//2º exemplo
+//let numeroDeAlunos = 10;
+
+   // for (let contador = 0; contador <= numeroDeAlunos; contador++){
+      //  console.log(contador)
+
+ // if(contador ==0){
+        //console.log("O número atual é zero")
+        
+    //}else if(contador % 2 ==0){
+
+       // console.log("O número"+ contador + "é Par")
+
+//}else{
+      //  console.log(`O número ${contador}é Impar`)
+    //}
+   // }  
+
+--------------------------------------------------------------------------------------------
+//1º exemplo
+//let numeroDeAlunos = 10;
 
 
    // for (let index = 0; index < arrayBuffer.length; index++){
