9bf7b45c29fa3c7a8f824b4afd4a8691b6af87c0
Lorena Oliveira <51932176+lorena-767@users.noreply.github.com>
2021-12-12 18:31:13 -0300

    Atividade encontro remoto2
    Atividade encontro remoto 2 com o uso de while e for

---
+++ sistemaw.js
@@ -0,0 +1,29 @@
+let nomeDeAluno = ["MARCEL", "LORENA", "LUANA"]
+
+for (let nome of nomeDeAluno){
+    console.log(`essa pessoa se chama ${nome}`)
+}
+
+//let numeroDeAlunos = 10;
+
+//let contador =0;
+
+//while(contador <= numeroDeAlunos){
+
+   // if(contador ==0){
+        //console.log("O número atual é zero")
+        
+    //}else if(contador % 2 ==0){
+
+       // console.log("O número"+ contador + "é Impar")
+
+//}else{
+      //  console.log(`O número ${contador}é par`)
+    //}
+       //contador++; 
+
+
+
+   // for (let index = 0; index < arrayBuffer.length; index++){
+      //  const element = array[index];
+   // }  
